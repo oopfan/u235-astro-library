4aa55a0f80a98bc82ef7772a91801e5642bc92fd
oopfan <oopfan@optonline.net>
2020-06-09 09:37:31 -0400

    Formulae for Airmass and Atmospheric Extinction.

--- projects/u235-astro/src/lib/u235-astro.service.ts
+++ projects/u235-astro/src/lib/u235-astro.service.ts
@@ -7,6 +7,36 @@ export class U235AstroService {
 
   constructor() { }
 
+  // Uses the secant method for calculating airmass:
+  calculateAirmass(altitudeDegrees: number): number {
+    if (altitudeDegrees <= 0) {
+      return Number.NaN;
+    }
+    const zenithAngle = 90 - Math.min(90, altitudeDegrees);
+    const airmass = 1 / Math.cos(zenithAngle / 180 * Math.PI);
+    return airmass;
+  }
+
+  //////////////////////////////////////////////////////////////////////////
+  // Extinction coefficients were derived from Al Kelly's table found here:
+  // http://kellysky.net/White%20Balancing%20RGB%20Filters.pdf
+  // A linear fit was made of his data points from altitude 30 to 90 degrees.
+  // Excellent fit but with a drop-off in accuracy below 30 degrees.
+  //
+  calculateRedExtinction(airmass: number): number {
+    const RxC = 0.9155 + 0.0845 * airmass;
+    return RxC;
+  }
+  calculateGreenExtinction(airmass: number): number {
+    const GxC = 0.8303 + 0.1697 * airmass;
+    return GxC;
+  }
+  calculateBlueExtinction(airmass: number): number {
+    const BxC = 0.7554 + 0.2446 * airmass;
+    return BxC;
+  }
+  //////////////////////////////////////////////////////////////////////////
+
   encodeAngleToMath(dec: number[]): number {
     // Converts a decoded angle to decimal degrees.
     //  dec is an array of five integers:
