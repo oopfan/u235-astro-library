908aa110b580696599b229ed9fe4c4ad10de6019
Brian Morgan <kamisan@optonline.net>
2020-07-11 12:06:38 -0400

    Init MoonStatsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MoonStatsComponent } from './moon-stats.component';
 import { U235AstroModule } from 'u235-astro';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoonStatsComponent
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
---
+++ src/app/moon-stats.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-moon-stats',
+    templateUrl: './moon-stats.component.html',
+    styleUrls: [ './moon-stats.component.css' ]
+})
+export class MoonStatsComponent implements OnInit {
+    ngOnInit(): void {
+    }
+}
